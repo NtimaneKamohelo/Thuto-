d52711c466d3041d0aedaa9c243abacf910c1778
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-09-17 13:20:03 +0200

    Created springboot backend

---
+++ thuto_backend/src/main/java/com/thutoEdu/thuto_backend/ThutoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thutoEdu.thuto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThutoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThutoBackendApplication.class, args);
+	}
+
+}
---
+++ thuto_backend/src/test/java/com/thutoEdu/thuto_backend/ThutoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thutoEdu.thuto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThutoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
